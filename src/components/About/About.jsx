c649990e60a342c9b998bfe38ced6f76c440146e
Hainer Stefan <hainer.stefan@gmail.com>
2024-07-16 17:55:48 +0300

    mq & content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,23 +11,26 @@ const About = () => {
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemDescription}>
               <p>
-              &nbsp; &nbsp; Hi, I'm Stefan, a 29-years-old Front-End software developer with a passion for creating user-friendly
-                web experiences.I specialize in <span className={styles.tech}>HTML</span>,{" "}
+                &nbsp; &nbsp; Hi, I'm Stefan, a 29-years-old Front-End software developer with a passion for creating
+                user-friendly web experiences.I specialize in <span className={styles.tech}>HTML</span>,{" "}
                 <span className={styles.tech}>CSS</span>, <span className={styles.tech}>Javascript</span>, and have some
                 experience with <span className={styles.tech}>React</span>. I also like to use frameworks like Tailwind,
-                Bootstrap to make the design process more efficient and consistent. <br/><br/> &nbsp;&nbsp; I'm constantly learning and keeping up with the latest web technologies to deliver high-quality, responsive, and interactive web applications<span className={styles.dot}>...</span><span className={styles.aboutTypingBlink}> </span>
+                Bootstrap to make the design process more efficient and consistent. <br />
+                <br /> &nbsp;&nbsp; I'm constantly learning and keeping up with the latest web technologies to deliver
+                high-quality, responsive, and interactive web applications<span className={styles.dot}>...</span>
+                <span className={styles.aboutTypingBlink}> </span>
               </p>
             </div>
           </li>
+          <hr />
 
           <li className={styles.aboutItem}>
             <div>
-            
-              <h3 className={styles.certificationsTitle}><img src={getImageUrl("about/web-design.png")} alt="certificate-icon" className={styles.certificateIcon} />Certifications</h3>
+              <h3 className={styles.certificationsTitle}>Certifications</h3>
 
               <div className={styles.certificationscontainer}>
                 <div className={styles.certificationcard}>
-                  <p>Responsive Web Design Development</p>
+                  <p className={styles.certificationTitle}>Responsive Web Design Development</p>
                   <h4 className={styles.freecodetext}>freecodecamp.org</h4>
                   <a
                     href="https://freecodecamp.org/certification/HainerStefan/responsive-web-design"
@@ -38,7 +41,9 @@ const About = () => {
                   </a>
                 </div>
                 <div className={styles.certificationcard}>
-                  <p>JavaScript Algorithms and Data Structures</p>
+                  <p className={styles.certificationTitle}>
+                    JavaScript Algorithms and <br /> Data Structures
+                  </p>
                   <h4 className={styles.freecodetext}>freecodecamp.org</h4>
                   <a
                     href="https://freecodecamp.org/certification/HainerStefan/javascript-algorithms-and-data-structures-v8"
