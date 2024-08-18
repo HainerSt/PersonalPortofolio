726af7af301734ef4f9a5435fa67daf464d0fc2a
HainerSt <hainer.stefan@gmail.com>
2024-08-18 22:17:58 +0300

    certif...

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./About.module.css";
+import certifications from "../../data/certifications.json";
 
 const About = () => {
   return (
@@ -10,16 +11,17 @@ const About = () => {
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemDescription}>
               <h4 className={styles.paragraphDesc}>
-              &nbsp;&nbsp;Hi, I'm Stefan, a 29-years-old Front-End software developer with a passion for creating
-                user-friendly web experiences.
+                &nbsp;&nbsp;&nbsp;&nbsp;Hi, I'm Stefan, a 29-years-old Front-End software developer with a passion for
+                creating user-friendly web experiences.
                 <br />
-                <br/>
-              &nbsp;&nbsp;I specialize in <span className={styles.tech}>HTML</span>,{" "}
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;I specialize in <span className={styles.tech}>HTML</span>,{" "}
                 <span className={styles.tech}>CSS</span>, <span className={styles.tech}>Javascript</span>, and have some
                 experience with <span className={styles.tech}>React</span>. I also like to use frameworks like Tailwind,
                 Bootstrap to make the design process more efficient and consistent. <br />
-                <br /> &nbsp;&nbsp; I'm constantly learning and keeping up with the latest web technologies to deliver
-                high-quality, responsive, and interactive web applications<span className={styles.dot}>...</span>
+                <br /> &nbsp;&nbsp;&nbsp;&nbsp; I'm constantly learning and keeping up with the latest web technologies
+                to deliver high-quality, responsive, and interactive web applications
+                <span className={styles.dot}>...</span>
                 <span className={styles.aboutTypingBlink}> </span>
               </h4>
             </div>
@@ -31,30 +33,19 @@ const About = () => {
               <h3 className={styles.certifTitle}>Certifications</h3>
 
               <div className={styles.certificationscontainer}>
-                <div className={styles.certificationcard}>
-                  <p className={styles.certificationTitle}>Responsive Web Design Development</p>
-                  <h4 className={styles.freecodetext}>freecodecamp.org</h4>
-                  <a
-                    href="https://freecodecamp.org/certification/HainerStefan/responsive-web-design"
-                    target="_blank"
-                    className={styles.freecodelink}
-                  >
-                    Open Certificate
-                  </a>
-                </div>
-                <div className={styles.certificationcard}>
-                  <p className={styles.certificationTitle}>
-                    JavaScript Algorithms and <br /> Data Structures
-                  </p>
-                  <h4 className={styles.freecodetext}>freecodecamp.org</h4>
-                  <a
-                    href="https://freecodecamp.org/certification/HainerStefan/javascript-algorithms-and-data-structures-v8"
-                    target="_blank"
-                    className={styles.freecodelink}
-                  >
-                    Open Certificate
-                  </a>
-                </div>
+                {certifications.map((certification, id) => {
+                  return (
+                    <div className={styles.certificationscontainer} key={id} certification={certification}>
+                      <div className={styles.certificationcard}>
+                        <p className={styles.certificationTitle}>{certification.title}</p>
+                        <h4 className={styles.freecodetext}>{certification.certification}</h4>
+                        <a href={certification.link} target="_blank" className={styles.freecodelink}>
+                          Open Certificate
+                        </a>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </li>
