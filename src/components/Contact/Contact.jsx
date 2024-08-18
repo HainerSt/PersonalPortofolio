c06c5405caee85d59402f7008a964f5e62eb5ba2
HainerSt <hainer.stefan@gmail.com>
2024-08-18 22:27:50 +0300

    ,,,

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,15 +12,21 @@ const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="mail icon" />
-          <a href="mailto:hainer.stefan@gmail.com">hainer.stefan@gmail.com</a>
+          <a target="_blank" href="mailto:hainer.stefan@gmail.com">
+            hainer.stefan@gmail.com
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/linkedinIcon.png")} alt="linkedin icon" />
-          <a href="https://www.linkedin.com/in/hainerstefan/">linkedin.com/in/hainerstefan</a>
+          <a target="_blank" href="https://www.linkedin.com/in/hainerstefan/">
+            linkedin.com/in/hainerstefan
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="github icon" />
-          <a href="https://github.com/hainerSt/">github.com/hainerSt</a>
+          <a target="_blank" href="https://github.com/hainerSt/">
+            github.com/hainerSt
+          </a>
         </li>
       </ul>
     </footer>
